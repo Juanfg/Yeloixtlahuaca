58ecbd20351c23a39fa55ad2f9913df9a18d297e
Juan Pablo Flores Galindo <juanfg@outlook.com>
2016-10-04 22:06:20 -0500

    Se agregaron Seeders de Actividades y Secciones

--- Yeloixtlahuaca/database/seeds/DatabaseSeeder.php
+++ Yeloixtlahuaca/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        //$this->call(UsersTableSeeder::class);
+    	$this->call(SeedSecciones::class);
+    	$this->call(SeedActividades::class);
     }
 }
